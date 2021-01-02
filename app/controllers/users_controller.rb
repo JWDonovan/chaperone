99f777334c07c64a1d3c123a4730dbb4bf8acbf6
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-01 22:23:17 -0500

    updated users controller with basic session support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
   get "/login" do
-    # if logged in, redirect to /users/:id or :slug
-    # else, show login form
-    erb :"users/login"
+    if !logged_in?
+      erb :"users/login"
+    else
+      redirect :"/users/#{ current_user.id }"
+    end
   end
 
   post "/login" do
     @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
-      "Logged in"
+      session[:user_id] = @user.id
+      redirect :"/users/#{ @user.id }"
     else
       redirect :"/login"
     end
+  end
+
+  get "/users/:id" do
+    "Users page"
+  end
+
+  get "/logout" do
+    session.destroy
+    redirect :"/login"
   end
 end
