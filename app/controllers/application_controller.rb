02d6fedb91cddbdb076e3d6f98c0a37bc2b11d41
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-01 22:22:02 -0500

    added session support and user login helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < Sinatra::Base
   configure do
     set :views, "app/views"
+    enable :sessions
+    set :session_secret, "super_secret"
   end
 
   get "/" do
     "Hello, world!"
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
   end
 end
