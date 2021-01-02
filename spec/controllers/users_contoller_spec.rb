ebd12e462fd9183a3c77098cf7b4e3e63727bfa7
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-02 03:11:01 -0500

    created basic users controller spec

---
+++ spec/controllers/users_contoller_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe UsersController do
+  context "get login" do
+    before do
+      get "/login"
+    end
+
+    it "responds with status code 200" do
+      expect(last_response.status).to eq(200)
+    end
+  end
+end
