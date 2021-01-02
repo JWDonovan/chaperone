c5d75de846c69e110dfa6ef36ecf1607e7201d48
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-02 03:11:19 -0500

    created basic users model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe User do
+  before do
+    @user = User.create(first_name: "Test", last_name: "User", email: "test@gmail.com", password: "password123")
+  end
+
+  it "has a secure password" do
+    expect(@user.authenticate("wrongpassword")).to eq(false)
+    expect(@user.authenticate("password123")).to eq(@user)
+  end
+end
